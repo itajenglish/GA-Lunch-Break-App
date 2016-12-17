4a6709ef79afbc342a04567fbc769ff4019ce7b6
Taj English <tajenglish@gmail.com>
2016-12-16 23:00:52 -0500

    1st Commit

---
+++ db/migrate/20161217034636_create_places.rb
@@ -0,0 +1,11 @@
+class CreatePlaces < ActiveRecord::Migration[5.0]
+  def change
+    create_table :places do |t|
+      t.string :latitude
+      t.string :longitude
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
