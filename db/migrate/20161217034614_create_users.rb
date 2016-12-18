ec7ed08c35699946345ebca087743dbf9cbdd482
Taj English <tajenglish@gmail.com>
2016-12-17 23:45:13 -0500

    change migration file

--- db/migrate/20161217034614_create_users.rb
+++ db/migrate/20161217034614_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
+      t.string :encrypted_password
+      t.string :salt
 
       t.timestamps
     end
