3fe533550d5ea479ce91d629903c103355c4138a
Jayan Zaman <jayanzaman@gmail.com>
2016-12-19 10:53:37 -0500

    got the user auth to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "places#index"
   resources :places
--- db/migrate/20161217034636_create_places.rb
+++ db/migrate/20161217034636_create_places.rb
@@ -2,10 +2,7 @@ class CreatePlaces < ActiveRecord::Migration[5.0]
   def change
     create_table :places do |t|
       t.string :name
-<<<<<<< HEAD
       t.string :address
-=======
->>>>>>> 16da0f1f833fa139ada59ba68484a8f69444a3af
       t.string :latitude
       t.string :longitude
       t.references :user, foreign_key: true
