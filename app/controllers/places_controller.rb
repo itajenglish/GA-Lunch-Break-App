cac38249da65e04d9502c8b79640bf815d86e914
Matthew Cabezon <matt.cabezon@gmail.com>
2016-12-19 11:48:42 -0500

    add maps and some views for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,34 @@
 class PlacesController < ApplicationController
 
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+    @comments = Comment.where(place_id = params[:id])
+  end
+
+  def create
+    place = params['place']
+    Place.create(name: place['name'],
+                  address: place['address'],
+                  latitude: place['latitude'],
+                  longitude: place['longitude'])
+    redirect_to(:back)
+  end
+
+  def destroy
+    Place.destroy(params[:id])
+  end
+
+  def update
+    place = params['place']
+    Place.update(name: place['name'],
+                  address: place['address'],
+                  latitude: place['latitude'],
+                  longitude: place['longitude'])
+    redirect_to(:back)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 resources :users, only:[:index,:show,:create,:new]
+resources :comments
+resources :places
 end
