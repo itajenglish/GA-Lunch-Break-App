8f8e27136e2d4448f768c488f865826c40a87dbc
Jayan Zaman <jayanzaman@gmail.com>
2016-12-19 18:41:05 -0500

    update working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,6 +15,10 @@ skip_before_filter  :verify_authenticity_token
     redirect_to("/places")
   end
 
+  def edit
+    @info = Place.find_by(id: params[:id])
+  end
+
   def destroy
     Place.destroy(params['id'])
     redirect_to(:back)
@@ -26,16 +30,18 @@ skip_before_filter  :verify_authenticity_token
 
   def update
     @place = params['place']
-    Place.update(name: @place['name'],
+    Place.update( params['id'],
+                  name: @place['name'],
                   address: @place['address'],
                   latitude: @place['latitude'],
-                  longitude: @place['longitude'])
+                  longitude: @place['longitude'],
+                  user_id: 1)
 
     redirect_to("/places")
   end
 
-  def place_params
-    params(:place).permit(:name, :address, :latitude, :longitude)
-  end
+  # def place_params
+  #   params(:place).permit(:name, :address, :latitude, :longitude)
+  # end
 
 end
