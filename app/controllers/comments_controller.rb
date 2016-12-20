5b8b97b0c546f9861fea0dc08dbe62009c62685d
Matthew Cabezon <matt.cabezon@gmail.com>
2016-12-20 18:28:53 -0500

    fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,29 @@
 class CommentsController < ApplicationController
 
+  def index
+    @comments = Comment.all
+  end
+
+  def create
+    Comment.create(content: params[:comment],user_id: session[:user_id],place_id:params[:id])
+    redirect_to(:back)
+  end
+
+  def show
+   @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def destroy
+    Comment.destroy(params['id'])
+    redirect_to(:back)
+  end
+
+  def update
+
+  end
+
 end
