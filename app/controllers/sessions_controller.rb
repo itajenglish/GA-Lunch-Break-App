f497a5adc61580b352fd538d8578945105dba1b2
Taj English <tajenglish@gmail.com>
2016-12-19 20:39:31 -0500

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   before_action :authenticate_user, :only => [:home, :profile, :setting]
   before_action :save_login_state, :only => [:login, :login_attempt]
-  # def login
-  #   #Login Form
-  # end
   def login
+    #Login Form
+  end
+  def login_attempt
     authorized_user = User.authenticate(params[:name_or_email],params[:login_password])
     if authorized_user
       flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.name}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/login'
+  post 'sessions/login_attempt'
 
   get 'sessions/home'
 
@@ -8,8 +9,8 @@ Rails.application.routes.draw do
   get 'sessions/setting'
 
   match 'signup', to: 'users#new' , :via => [:get, :post]
-  # match 'login', to: 'sessions#login', :via => [:get, :post]
-  post 'sessions/login', to: 'sessions#login'
+  match 'login', to: 'sessions#login_attempt', :via => [:get, :post]
+  # post 'sessions/login', to: 'sessions#login'
   match 'logout', to: 'sessions#logout', :via => [:get, :post]
   match 'home', to: 'sessions#home', :via => [:get, :post]
   match 'profile', to: 'sessions#profile', :via => [:get, :post]
